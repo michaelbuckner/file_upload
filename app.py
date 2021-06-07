ed412e10bf3fa0b937d929378b6a6abe551e065d
michaelbuckner <mike@mikebuckner.me>
2021-06-07 09:49:25 -0400

    added debug

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask
 UPLOAD_FOLDER = '/home/ec2-user'
 
 app = Flask(__name__)
-app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.config['DEBUG'] = True
